2223c9d0ee1809f3ae99f3a1d19d1588c5fa3ad5
Chris Moreton <chris@netsensia.com>
2020-09-14 13:19:33 +0100

    Improve publish script

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
+sed -i "s/def engine_version = .*/def engine_version = '$1'/g" build.gradle
 ./gradlew build
-cp build/libs/rivalchess-$1.jar ~/Chess/ && chmod +x ~/Chess/*.jar
+cp build/libs/rivalchess-$1-1.jar ~/Chess/ && chmod +x ~/Chess/*.jar
