34db21a5133ab0faf122fe04e54d9ecf796f6924
Chris Moreton <chris.moreton@hmcts.net>
2020-09-17 17:56:33 +0100

    Version 31.0.11-3

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 sed -i "s/def engine_version = .*/def engine_version = '$1'/g" build.gradle
-sed -i "s/def uci_version = .*/def uci_version = '$2'/g" build.gradle
+sed -i "s/def uci_build = .*/def uci_build = '$2'/g" build.gradle
 ./gradlew build
 git add -A
 git commit -m "Version $1-$2"
